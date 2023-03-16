ec6aa93c9a0cd180915d3ea94b6db7c855690a88
EthanHunt404 <etbuttazzi@hotmail.com>
2023-03-16 03:52:35 -0300

    Initial commit

---
+++ src/main/java/net/croma/mod/block/RedChromaSlabBlock.java
@@ -0,0 +1,30 @@
+
+package net.croma.mod.block;
+
+import org.checkerframework.checker.units.qual.s;
+
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.block.state.properties.SlabType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.SlabBlock;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.List;
+import java.util.Collections;
+
+public class RedChromaSlabBlock extends SlabBlock {
+	public RedChromaSlabBlock() {
+		super(BlockBehaviour.Properties.of(Material.METAL).sound(SoundType.NETHERITE_BLOCK).strength(1.3f, 1600f).lightLevel(s -> 2).hasPostProcess((bs, br, bp) -> true).emissiveRendering((bs, br, bp) -> true).dynamicShape());
+	}
+
+	@Override
+	public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+		List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+		if (!dropsOriginal.isEmpty())
+			return dropsOriginal;
+		return Collections.singletonList(new ItemStack(this, state.getValue(TYPE) == SlabType.DOUBLE ? 2 : 1));
+	}
+}
